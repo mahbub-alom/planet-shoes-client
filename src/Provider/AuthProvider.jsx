4b8f5881bd9af22fdd4fb35b90a88badbd171718
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-06-04 01:34:38 +0600

    firebase config setup done

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,90 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+// import axios from "axios";
+
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
+
+// eslint-disable-next-line react/prop-types
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // create user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // google sign in
+  const googleProvider = new GoogleAuthProvider();
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  // update user information
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+  // Log in
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // Log out
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // get and set token
+      setUser(currentUser);
+      setLoading(false);
+      console.log(currentUser);
+      // if (currentUser) {
+      //     axios.post('https://assignment-twelve-server-smoky.vercel.app/jwt', { email: currentUser.email })
+      //         .then(data => {
+      //             localStorage.setItem('access-token', data.data.token);
+      //             setLoading(false);
+      //         })
+      //         .catch(error => {
+      //             console.error('Failed to get JWT token:', error);
+      //             setLoading(false);
+      //         });
+      // } else {
+      //     localStorage.removeItem('access-token');
+      //     setLoading(false);
+      // }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    googleSignIn,
+    updateUserProfile,
+    signIn,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
